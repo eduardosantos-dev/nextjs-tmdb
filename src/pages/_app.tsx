72b779f6244a2379be5827cce96a990025cfcc1b
Eduardo Santos <eduardosantos.ist@gmail.com>
2022-10-05 12:00:00 -0300

    Fix analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,37 +11,53 @@ import { SidebarDrawerProvider } from "../context/SidebarDrawerContext";
 import { queryClient } from "../services/queryClient";
 import "../styles/global.scss";
 import * as gtag from "../lib/gtag";
+import Script from "next/script";
 
 const isProduction = process.env.NODE_ENV === "production";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
-
   useEffect(() => {
     const handleRouteChange = (url: URL) => {
-      /* invoke analytics function only for production */
-      if (isProduction) gtag.pageview(url);
+      gtag.pageview(url);
     };
     router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
     };
   }, [router.events]);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Chakra cookies={pageProps.cookies}>
-        <SidebarDrawerProvider>
-          <VideoModalProvider>
-            <Head>
-              <title>tmdb</title>
-            </Head>
-            <Component {...pageProps} />
-          </VideoModalProvider>
-        </SidebarDrawerProvider>
-      </Chakra>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+      <QueryClientProvider client={queryClient}>
+        <Chakra cookies={pageProps.cookies}>
+          <SidebarDrawerProvider>
+            <VideoModalProvider>
+              <Head>
+                <title>tmdb</title>
+              </Head>
+              <Component {...pageProps} />
+            </VideoModalProvider>
+          </SidebarDrawerProvider>
+        </Chakra>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </>
   );
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,5 @@
 // eslint-disable-next-line @next/next/no-document-import-in-page
 import Document, { Head, Html, Main, NextScript } from "next/document";
-
-const isProduction = process.env.NODE_ENV === "production";
 export default class MyDocument extends Document {
   render() {
     return (
@@ -13,28 +11,6 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
-
-          {isProduction && (
-            <>
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-              />
-              <script
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                  __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-                }}
-              />
-            </>
-          )}
         </Head>
         <body>
           <Main />
